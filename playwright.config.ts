f78efdfec5e80578db848d9bf3ec6e1fc4db14e3
Cedrec Rosas <cedrecrosas@gmail.com>
2026-06-25 12:27:06 +1200

    test4

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,13 +1,16 @@
-// playwright.config.ts
 import { defineConfig } from '@playwright/test';
 import { defineBddConfig } from 'playwright-bdd';
 
 const testDir = defineBddConfig({
   features: 'features/*.feature',
   steps: 'steps/*.ts',
-  importTestFrom: 'fixtures.ts', // ← path relative to config file
+  importTestFrom: 'fixtures.ts',
 });
 
 export default defineConfig({
   testDir,
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['list'],
+  ],
 });
